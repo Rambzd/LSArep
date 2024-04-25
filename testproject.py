86529ef5e3bb0470a3cd239f98da270213caec71
Rambzd <rambzd@gmail.com>
2024-04-25 15:11:03 +0200

    Latent Semantic Analysis Tests

---
+++ testproject.py
@@ -0,0 +1,8 @@
+stundenlohn = input("Geben Sie Ihre Stundenlohn ein: ")
+tag = 8 * int(stundenlohn)
+monat = tag * 22
+
+print("Du verdienst " +str(stundenlohn)+ " Euro pro Stunde")
+print("Du verdienst " +str(tag)+ " Euro pro Tag")
+print("Du verdienst " +str(monat)+ " Euro pro Monat")
+input("Druecken Sie eine beliebige Taste")
